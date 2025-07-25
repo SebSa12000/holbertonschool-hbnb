add937db1569c829dc7946f51305c27536145c81
SebSa12000 <sebastien.salgues@gmail.com>
2025-07-25 13:34:09 +0200

    get place id

--- part4/js/scripts.js
+++ part4/js/scripts.js
@@ -4,8 +4,25 @@
 */
 
 document.addEventListener('DOMContentLoaded', () => {
-    /* DO SOMETHING */
-    fetchPlaces('');
+
+
+    // Ftech detailed place if token and place id identified
+    
+    const urlParams = new URLSearchParams(window.location.search);
+    const placeId = urlParams.get("id");
+    
+    try {
+      if (placeId) {
+        fetchDetailedPlace('', placeId);
+      }
+      else
+      {
+        /* DO SOMETHING */
+        fetchPlaces('');
+      }
+    } catch (error) {
+      console.error(error);
+    }
   });
 
 /** Places fetch and display */
@@ -46,3 +63,55 @@ function displayPlaces(places) {
   /*applyPriceFilter();*/
 }
 
+/** Place Details Fetch and Display */
+async function fetchDetailedPlace(token, placeId) {
+  try {
+    const response = await fetch(
+      `http://127.0.0.1:5000/api/v1/places/${placeId}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    if (response.ok) {
+      const detailedPlace = await response.json();
+      displayDetailedPlaces(detailedPlace);
+    } else {
+      console.error("Failed to fetch detailed place.");
+    }
+  } catch (error) {
+    console.error("Error fetching place detail:", error);
+  }
+}
+
+function displayDetailedPlaces(place) {
+  document.getElementById("place-details").innerHTML = `
+        <h1 class="detailedTitle">${place.title}</h1>
+        <p class="detailedDescription">${place.description}</p>
+        <p class="amenities">What this place offers: 
+
+        </p>
+        <div class='addButtonContainer'><a href="add_review.html?id=${
+          place.id
+        }"><button>Add a review</button></a></div>
+    `;
+
+  const reviewsPlace = document.getElementById("reviews");
+
+  if (place.reviews && place.reviews.length > 0) {
+    place.reviews.forEach((review) => {
+      const reviewCard = document.createElement("div");
+      reviewCard.classList.add("review-card");
+      reviewCard.innerHTML = `
+                <p>${review.text}</p>
+                <p><strong>Rating: ${review.rating}/5</strong></p>
+            `;
+      reviewsPlace.appendChild(reviewCard);
+    });
+  } else {
+    reviewsPlace.innerHTML += "<p>No reviews available for this place.</p>";
+  }
+  //initializeCarousel();
+}
